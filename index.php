fa9ec2fb5de1065cf59c1a09733232a35fe78ea8
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-09-17 14:03:32 +0200

    [UPD] Created a new class called "Movie" with some instance's variables

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-    
+    //creo una nuova classe e la chiamo "movie"
+    class Movie {
+
+        //dichiaro delle variabili di istanza
+        public $title;
+        public $director;
+        public $genre;
+        public $description;
+        public $length;
+    }
 
 ?>
